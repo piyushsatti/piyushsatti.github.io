faff14bc623764b673666fde0607609ec1031110
piyushsatti <piyushsatti@gmail.com>
2025-09-23 20:47:06 -0400

    Updated site home path to "/"

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,9 +4,9 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://dennisklappe.github.io',
+  site: 'https://piyushsatti.github.io',
   // Only use base path in production (GitHub Pages)
-  base: process.env.NODE_ENV === 'production' ? '/astro-theme-terminal' : '/',
+  base: process.env.NODE_ENV === 'production' ? '/' : '/',
   integrations: [sitemap()],
   markdown: {
     shikiConfig: {
